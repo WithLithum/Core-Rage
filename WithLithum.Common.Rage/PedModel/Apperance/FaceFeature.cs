55c6d7761026f071beef06b50af1df360cd83d45
WithLithum <RelaperCrystal@163.com>
2021-09-27 10:38:38 +0800

    :construction: Add face feature customization

---
+++ WithLithum.Common.Rage/PedModel/Apperance/FaceFeature.cs
@@ -0,0 +1,39 @@
+﻿using Rage;
+using Rage.Native;
+
+namespace WithLithum.Core.PedModel.Apperance
+{
+    /// <summary>
+    /// Provides utilties to manipulate a specific face feature of freemode model head blends.
+    /// </summary>
+    public class FaceFeature
+    {
+        private readonly HeadBlendCustomizer _customizer;
+
+        internal FaceFeature(HeadBlendCustomizer customizer, FaceFeatureType type)
+        {
+            Type = type;
+            _customizer = customizer;
+        }
+
+        /// <summary>
+        /// Gets the type of the feature that is manipulate by this instance.
+        /// </summary>
+        /// <value>
+        /// The type of this instance.
+        /// </value>
+        public FaceFeatureType Type { get; }
+
+        private Ped Ped => _customizer.BasePed;
+
+        /// <summary>
+        /// Sets the value of this instance.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public void SetValue(float value)
+        {
+            NativeFunction.Natives.x71A5C1DBA060049E(Ped, (int)Type, value);
+        }
+
+    }
+}
---
+++ WithLithum.Common.Rage/PedModel/Apperance/HeadBlendCustomizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Rage;
+using Rage.Exceptions;
+
+namespace WithLithum.Core.PedModel.Apperance
+{
+    public class HeadBlendCustomizer
+    {
+        public HeadBlendCustomizer(Ped ped)
+        {
+            if (ped == null) throw new ArgumentNullException(nameof(ped));
+            if (!ped.IsValid()) throw new InvalidHandleableException(ped);
+
+            BasePed = ped;
+        }
+
+        public Ped BasePed { get; }
+    }
+}
